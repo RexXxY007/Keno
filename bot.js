f6c12f6bbb83e5b67636ba113f2a7da025b9d96d
RexXxY007 <61401220+RexXxY007@users.noreply.github.com>
2020-02-24 18:07:08 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,16 @@
 const Discord = require('discord.js');
 const bot = new Discord.client();
 
-client.login("NjgxNDE3MzgyMDQ4NDMyMTg2.XlOJ9w.xZfwgu8C0ZPSko5fUg3TVlKIGxY");
-
 bot.on('ready', () =>{
     console.log('The bot is online!');
 });
 
 client.on('message', message =>{
-    if(message.author.bot)
-    return;
-
-    if(message.content.toLowerCase() === 'hello')
-    message.channel.send("Hey!");
+    if (message.content === 'ping') {
+        message.reply('pong');
+    }
 });
+
+
+// THIS MUST BE THIS WAY
+client.login(process.env.BOT_TOKEN);
